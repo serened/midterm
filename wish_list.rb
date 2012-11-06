4052667e811125296a9fcecc71a214cd55162c9d
serened <serened@gmail.com>
2012-11-06 09:14:18 -0800

    passing first test

--- wish_list.rb
+++ wish_list.rb
@@ -1,12 +1,15 @@
 class WishList
+	include Enumerable
+
 attr_accessor :wishes
 
 def initialize
 	@wishes = wishes
 end
 
-def wishes
-	@wishes.to_enum
+def map
+	[].each {|w| ids_w[w] = wish_list}
+	list.collect{ |w| ids_w[w]}
 end
 
 end
