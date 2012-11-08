c4d9accfcb1912349e1307ed29c4669729d90670
serened <serened@gmail.com>
2012-11-07 17:08:43 -0800

    updaing not totally functional midterm files

--- thanksgiving_dinner.rb
+++ thanksgiving_dinner.rb
@@ -10,30 +10,33 @@ attr_accessor :diet, :guests
 	end
 
 	def guests
-		@guests = guests
+		@guests = guests.to_s.size
 	end
 
 	def seating_chart_size
-		seating_chart_size.inject{print guests.to_s.length } 
+		seating_chart_size.inject{:guests.to_s.size} 
 	end
 end
 
 module Menu
 	attr_accessor :proteins, :veggies
 
-	def initialize
-		@proteins = proteins
-		@veggies = veggies
+	# initialize
+	#	@proteins = proteins
+	#	@veggies = veggies
+	#end
+	
+	def menu
 	end
 	
-	def foods
-		@proteins = ["Tofurkey", "Hummus"]
-		@veggies = [:ginger_carrots ,:potatoes, :yams].map{|v| v.to_s.capitalize }
-	end
+	#def foods
+	#	@proteins = ["Tofurkey", "Hummus"]
+	#	@veggies = [:ginger_carrots ,:potatoes, :yams].map{|v| v.to_s.capitalize }
+	#end
 	
-	def whats_for_dinner
+	#def whats_for_dinner
 		puts "Tonight we have proteins #{@proteins} and veggies #{@veggies}."		
-	end
+	#end
 end
 
 class ThanksgivingDinner
