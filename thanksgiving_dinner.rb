eb501a45f471d63493fede61a79b4d35c0223151
serened <serened@gmail.com>
2012-11-08 08:25:28 -0800

    another passing test

--- thanksgiving_dinner.rb
+++ thanksgiving_dinner.rb
@@ -1,9 +1,11 @@
 module Dinner
-attr_accessor :diet, :guests
+attr_accessor :diet, :guests, :proteins, :veggies
 
 	def initialize(diet)
 		@diet = diet
 		@guests = guests
+		@proteins = ["Tofurkey", "Hummus"].join(' and ').insert(-1, ',')
+		@veggies = [:ginger_carrots ,:potatoes, :yams].map{|v| v.to_s.tr("_", " ").capitalize }.join(', ').sub(', Yams', ', and Yams').sub('Ginger carrots, ','Ginger Carrots, ')
 	end
 
 	def diet
@@ -17,32 +19,18 @@ attr_accessor :diet, :guests
 	def seating_chart_size
 		guests.inject(0){|memo,name| memo+name.size} 
 	end
-end
 
-module Menu
-	attr_accessor :proteins, :veggies
-
-	# initialize
-	#	@proteins = proteins
-	#	@veggies = veggies
-	#end
-	
 	def menu
+		
 	end
 
-	#def foods
-	#	@proteins = ["Tofurkey", "Hummus"]
-	#	@veggies = [:ginger_carrots ,:potatoes, :yams].map{|v| v.to_s.capitalize }
-	#end
-	
-	#def whats_for_dinner
-	#	puts "Tonight we have proteins #{@proteins} and veggies #{@veggies}."		
-	#end
+	def whats_for_dinner
+		 "Tonight we have proteins #{@proteins} and veggies #{@veggies}."		
+	end
 end
 
 class ThanksgivingDinner
 	include Dinner
-	include Menu
 
 	#attr_accessor :desserts
 
@@ -50,9 +38,9 @@ class ThanksgivingDinner
 	#	@desserts = desserts
 	#end
 
-	def menu
-		super
-		@desserts = {:pies => [:pumkin_pie], :other => ["Chocolate Moose"], :molds => [:cranberry, :mango, :cherry]}.map { |d, t| vs.map { |d| [k, v] } }.flatten(1)
-	end	
+	#def menu
+	#	super
+	#	@desserts = {:pies => [:pumkin_pie], :other => ["Chocolate Moose"], :molds => [:cranberry, :mango, :cherry]}.map { |d, t| vs.map { |d| [k, v] } }.flatten(1)
+	#end	
 
 end
