4ead862a7228ee984564a45ad6d8a34d76c60031
serened <serened@gmail.com>
2012-11-09 08:31:42 -0800

    passing another test

--- thanksgiving_dinner.rb
+++ thanksgiving_dinner.rb
@@ -1,17 +1,13 @@
 module Dinner
+
 attr_accessor :diet, :guests, :proteins, :veggies, :desserts
 
 	def initialize(diet)
-		@diet = diet
-		@guests = guests
-		@proteins = ["Tofurkey", "Hummus"].join(' and ').insert(-1, ',')
-		@veggies = [:ginger_carrots ,:potatoes, :yams].map{|v| v.to_s.tr("_", " ").capitalize }.join(', ').sub(', Yams', ', and Yams').sub('Ginger carrots, ','Ginger Carrots, ')
-		@desserts = {:pies => [:pumkin_pie], :other => ["Chocolate Moose"], :molds => [:cranberry, :mango, :cherry]}.collect_concat{|k,v| v.to_s}.flatten
-
-	end
-
-	def diet
 		@diet = :vegan
+		@guests
+		@proteins = ["Tofurkey", "Hummus"]
+		@veggies = [:ginger_carrots ,:potatoes, :yams]
+		@desserts
 	end
 
 	def guests
@@ -22,18 +18,21 @@ attr_accessor :diet, :guests, :proteins, :veggies, :desserts
 		guests.inject(0){|memo,name| memo+name.size} 
 	end
 
-	def menu
-	end	
-	
 	def whats_for_dinner
-		"Tonight we have proteins #{@proteins} and veggies #{@veggies}."		
+		@veggies = [:ginger_carrots ,:potatoes, :yams].map{|v| v.to_s.tr("_", " ").capitalize }.join(', ').sub(', Yams', ', and Yams').sub('Ginger carrots, ','Ginger Carrots, ')
+		@proteins = ["Tofurkey", "Hummus"].join(' and ').insert(-1, ',')
+		"Tonight we have proteins #{proteins} and veggies #{veggies}."		
 	end
 	def whats_for_dessert
+		@desserts = {:pies => [:pumkin_pie], :other => ["Chocolate Moose"], :molds => [:cranberry, :mango, :cherry]}.collect_concat{|k,v| v.to_s.tr(":",'').tr("[",'').tr("]",'')}.join
 		"Tonight we have 5 delicious desserts: #{@desserts}."
+	end
+
+	def menu
+		menu=Hash.new{|menu, diet| menu[diet] = :vegan }
 	end
 end
 
 class ThanksgivingDinner
 	include Dinner
-
 end
